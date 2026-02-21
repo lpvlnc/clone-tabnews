6b70afae0a2abf4df6d9b52d49d3ff16369aff7c
Leonardo Valença <43501353+lpvlnc@users.noreply.github.com>
2026-02-21 02:54:40 +0000

    feat: create `user.findOneByUsername` method and `/api/v1/users/[username]` endpoint

--- models/user.js
+++ models/user.js
@@ -1,5 +1,33 @@
 import database from "infra/database.js";
-import { ValidationError } from "infra/errors";
+import { ValidationError, NotFoundError } from "infra/errors";
+
+async function findOneByUsername(username) {
+  const userFound = await runSelectQuery(username);
+  return userFound;
+
+  async function runSelectQuery(username) {
+    const result = await database.query({
+      text: `
+            SELECT 
+              * 
+            FROM 
+              users 
+            WHERE 
+              LOWER(username) = LOWER($1)
+            LIMIT
+              1;
+              `,
+      values: [username],
+    });
+    if (result.rowCount === 0) {
+      throw new NotFoundError({
+        message: "O username informado não foi encontrado no sistema.",
+        action: "Verifique se o username foi digitado corretamente.",
+      });
+    }
+    return result.rows[0];
+  }
+}
 
 async function create(userInputValues) {
   await validateUniqueEmail(userInputValues.email);
@@ -66,6 +94,7 @@ async function create(userInputValues) {
 
 const user = {
   create,
+  findOneByUsername,
 };
 
 export default user;
